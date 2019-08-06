1458e39d40faa6db58009ff1e99583b660c6b962
Pedro Moraes <pedro.moraes@vgresiduos.com.br>
2019-08-06 14:55:19 -0300

    v1.3
    Adicionado uma página para criação de lista de compras (similar da página principal)

--- controllers/treinamento.js
+++ controllers/treinamento.js
@@ -7,9 +7,9 @@ angular.module("treinamentoModule", [])
         $scope.treinamentoObj = [];
         $scope.treinamentoObj.table = [];
         //Inicialização da tabela
-        $scope.treinamentoObj.table.push("Pedro");
+        /*$scope.treinamentoObj.table.push("Pedro");
         $scope.treinamentoObj.table.push("Algo");
-        $scope.treinamentoObj.table.push("Mais");
+        $scope.treinamentoObj.table.push("Mais");*/
 
         //Config da página
         $scope.treinamentoObj.title = "Início";
@@ -38,10 +38,33 @@ angular.module("treinamentoModule", [])
         $scope.addPerson = function(){
                 //Até então só funciona uma vez, não sei pq
                 if($scope.treinamentoObj.table.includes($scope.treinamentoObj.nome)){
+                        alert("Nome já na tabela!");
                         return;  
                 }
                 else{
-                $scope.treinamentoObj.table.push($scope.treinamentoObj.nome);
+                        $scope.treinamentoObj.table.push($scope.treinamentoObj.nome);
                 }
         };
+
+        $scope.deletePerson = function(){
+                if($scope.treinamentoObj.table.includes($scope.treinamentoObj.nome)){
+                        for (var i = 0; i < $scope.treinamentoObj.table.length; i++){
+                                if ($scope.treinamentoObj.table[i] == $scope.treinamentoObj.nome){
+                                        $scope.treinamentoObj.table.splice(i,1);
+                                }
+                        } 
+                }
+                else{
+                        alert("Nome não está na tabela");
+                        return;
+                }
+        };
+}])
+
+.controller("treinamentoListCtrl", ["$scope", function($scope){
+        var checked = false;
+        $scope.check = function(){
+                checked = !checked;
+        };
+
 }]);
