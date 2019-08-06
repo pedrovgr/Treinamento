9d001d41b34b1cb40ba9833861d91c8a6618c2f3
Pedro Moraes <pedro.moraes@vgresiduos.com.br>
2019-08-06 11:02:19 -0300

    v1.1
    Adicionado comentários pertinentes

--- controllers/treinamento.js
+++ controllers/treinamento.js
@@ -1,11 +1,17 @@
 angular.module("treinamentoModule", [])
 
+//possível adicionar vários controllers
+
 .controller("treinamentoCtrl", ["$scope", function($scope){
+        //Declara o objeto
         $scope.treinamentoObj = [];
         $scope.treinamentoObj.table = [];
+        //Inicialização da tabela
         $scope.treinamentoObj.table.push("Pedro");
         $scope.treinamentoObj.table.push("Algo");
         $scope.treinamentoObj.table.push("Mais");
+
+        //Config da página
         $scope.treinamentoObj.title = "Início";
         $scope.treinamentoObj.subtitle = "Trem bom demais da conta, sô!";
         $scope.treinamentoObj.clicks = 0;
@@ -13,6 +19,8 @@ angular.module("treinamentoModule", [])
         $scope.addClick = function(){
                 $scope.treinamentoObj.clicks += 1;
         };
+
+        //Pegar o dia atual
         var data = new Date;
         var dia = data.getDate();
         if (dia<10){
@@ -23,14 +31,12 @@ angular.module("treinamentoModule", [])
                 mes = "0" + mes;
         }
         var ano = data.getFullYear();
+        //Atualização do dia para o $scope
         $scope.treinamentoObj.date =   dia + "/" + mes + "/" + ano;
+
+        //Funções
         $scope.addPerson = function(){
+                //Até então só funciona uma vez
                 $scope.treinamentoObj.table.push($scope.treinamentoObj.nome);
         };
-        $scope.print = function(){
-                for (i in table){
-                        return true;
-                };
-        };
-
 }]);
